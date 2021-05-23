e89f6b6eb353c397c5b29a3c58baf13649a530b0
Gabriel Vendramini <gabriel_vendramini@hotmail.com>
2021-05-23 22:17:09 +0200

    repository update

--- src/main/java/com/wezaam/withdrawal/repository/WithdrawalRepository.java
+++ src/main/java/com/wezaam/withdrawal/repository/WithdrawalRepository.java
@@ -1,7 +1,13 @@
 package com.wezaam.withdrawal.repository;
 
-import com.wezaam.withdrawal.model.Withdrawal;
+import java.time.Instant;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.wezaam.withdrawal.model.Withdrawal;
+
 public interface WithdrawalRepository extends JpaRepository<Withdrawal, Long> {
+	
+	List<Withdrawal> findAllByExecuteAtBefore(Instant date);
 }
